81eaedce2c6534c4a8eee05cd92cbdacba54a6d7
Your Name <saul.vera787@gmail.com>
2024-03-19 14:52:11 -0400

    header push, it may need add or edit later

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
